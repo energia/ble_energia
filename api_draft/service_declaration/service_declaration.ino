b10cee61f9c9d4b1b0e7167412a1fa7ad9e95d95
Andrew Blatner <a0226940@ti.com>
2016-06-17 18:02:10 -0700

    Separate service declaration example into .ino, .h
    The way Energia compiles .ino files places function definitions before
    structs, so in order to use a struct as a parameter, it must be
    included in a .h file.

--- api_draft/service_declaration/service_declaration.ino
+++ api_draft/service_declaration/service_declaration.ino
@@ -1,27 +1,5 @@
 
-typedef struct 
-{
-  byte              UUIDlen; // in bytes, either 2 (16 bit) or 16 (128 bit)
-  byte              UUID[16]; // array of UUID bytes, little-endian 
-  byte              properties; // bitwise OR of macros: e.g. BLE_READABLE | BLE_WRITABLE
-  // Null terminated; internally set permissions to read only so we don't have to worry about the length changing
-  char              *charDesc;
-  // Client Characteristic Configuration Descriptor, enables/disables notifcations and indications to the client
-  byte              CCCD;
-  byte              valueFormat;
-  byte              valueExponent; // only used with integer formats, e.g. value = storedValue*10^valueExponent
-} BLE_Char;
-
-typedef struct
-{
-  byte              UUIDlen; // in bytes
-  byte              UUID[16]; // array of UUID bytes
-  BLE_Char          *chars;
-} BLE_Service;
-
-#define BLE_READABLE 0x02
-#define BLE_NOTIFY 0x01
-#define BLE_INT 14
+#include "ble.h"
 
 BLE_Char heartRateChar =
 {
@@ -40,18 +18,21 @@ BLE_Service heartRateService =
   allChars
 };
 
+int addService(BLE_Service* service)
+{
+  return 0xFFFF; // Fake handle
+}
+
+int registerService(int handle)
+{
+  return 0;
+}
 
 void setup()
 {
-  // put your setup code here, to run once:
-  Serial.begin(115200);
-  Serial.println(heartRateService.UUIDlen);
-  Serial.println(heartRateService.UUID[0]);
-  Serial.println(heartRateService.UUID[1]);
+  registerService(addService(&heartRateService));
 }
 
 void loop()
 {
-  // put your main code here, to run repeatedly:
-  
 }
