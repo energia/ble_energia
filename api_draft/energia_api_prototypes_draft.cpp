1418e8366425a99c61efcb23498d25b0460fcc67
Andrew Blatner <a0226940@ti.com>
2016-06-17 18:05:19 -0700

    Remove components shown in service declaration ex

--- api_draft/energia_api_prototypes_draft.cpp
+++ api_draft/energia_api_prototypes_draft.cpp
@@ -26,6 +26,7 @@ int ble.useProfile(BLE_Profile *profile); // For predefined profiles
 int ble.addService(BLE_Service *service);
 int ble.addCharacteristic(BLE_Char *characteristic, int serviceHandle);
 int ble.addCharacteristic(BLE_Char *characteristic); // Adds to last service
+int ble.registerService(BLE_Service *service);
 int ble.registerService(int handle);
 int ble.registerService(void); // Registers last service
 /*
@@ -72,32 +73,6 @@ typedef struct
   byte              numServices;
   BLE_Service       **services; 
 } BLE_Profile;
-
-/*
- * Example declaration, compiles in Energia:
-
-#define BLE_READABLE 0x02
-#define BLE_NOTIFY 0x01
-#define BLE_INT 14
-
-BLE_Char heartRateChar =
-{
-  2, {0x37, 0x2A}, // UUID for heart rate measurement, 16 bits
-  BLE_READABLE,
-  "Heart rate measurement",
-  BLE_NOTIFY,
-  BLE_INT, 0 // signed 16 bit integer (Energia int), multiplied by 10^0 = 1
-};
-
-BLE_Char *allChars = {&heartRateChar}; // Alternative to this would be to fix the nu
-
-BLE_Service heartRateService = 
-{
-  2, {0x0D, 0x18}, // UUID for heart rate service. 16 bits
-  allChars
-};
-
- */
 
 
 
