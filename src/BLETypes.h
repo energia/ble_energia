27b1de808047fa665ea1b82b70c0baad183ea541
Andrew Blatner <a0226940@ti.com>
2016-06-22 17:06:57 -0700

    Add header for user-facing macros and data types.

---
+++ src/BLETypes.h
@@ -0,0 +1,11 @@
+
+#ifndef BLE_TYPES_h
+#define BLE_TYPES_h
+
+#include <sap/source/ti/sap/sap.h>
+
+#define BLE_PORT_LOCAL          SAP_PORT_LOCAL // unsupported
+#define BLE_PORT_UART           SAP_PORT_REMOTE_UART
+#define BLE_PORT_SPI            SAP_PORT_REMOTE_SPI
+
+#endif
