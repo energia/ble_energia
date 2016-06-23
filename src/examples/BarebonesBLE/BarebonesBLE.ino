d882e6c013eb1882c7be165f8aebffb51c783ba7
Andrew Blatner <a0226940@ti.com>
2016-06-23 09:56:46 -0700

    Add example that blinks and inits BLE.

--- src/examples/BarebonesBLE/BarebonesBLE.ino
+++ src/examples/BarebonesBLE/BarebonesBLE.ino
@@ -1,11 +1,17 @@
 
 #include <ble.h>
+#define LED RED_LED
 
-void setup()
-{
+void setup() {
+  BLE ble = BLE();
+  ble.begin();
+  pinMode(LED, OUTPUT);     
 }
 
-void loop()
-{
+// the loop routine runs over and over again forever as a task.
+void loop() {
+  digitalWrite(LED, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(2000);               // wait for 100 ms
+  digitalWrite(LED, LOW);    // turn the LED off by making the voltage LOW
+  delay(2000);               // wait for 100 ms
 }
-
