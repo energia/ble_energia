41f7d579a7c2890c33a824cbd7d0fbe60b7886f8
Andrew Blatner <a0226940@ti.com>
2016-07-25 15:33:07 -0700

    Cleanup BLETypes.h

--- BLE/BLETypes.h
+++ BLE/BLETypes.h
@@ -3,29 +3,28 @@
 #define BLE_TYPES_H
 
 #include <sap.h>
-#include <hal_defs.h>
 
 #include <Energia.h>
 
 /* Energia BLE status codes */
-#define BLE_SUCCESS             SNP_SUCCESS // 0x00
-#define BLE_FAILURE             SNP_FAILURE // 0x83
-#define BLE_INVALID_PARAMETERS  SNP_INVALID_PARAMS // 0x84
-#define BLE_ALREADY_ADVERTISING SNP_ALREADY_ADVERTISING // 0x8A
-#define BLE_NOT_ADVERTISING     SNP_NOT_ADVERTISING // 0x8B
-#define BLE_NOT_CONNECTED       SNP_NOT_CONNECTED // 0x92
-#define BLE_INVALID_HANDLE      0x50
-#define BLE_UNDEFINED_VALUE     0x51
-#define BLE_NOT_IMPLEMENTED     0x52
-#define BLE_SERIAL_DISABLED     0x53
-#define BLE_TIMEOUT             0x54
-#define BLE_CHECK_ERROR         0x55
+#define BLE_SUCCESS                    SNP_SUCCESS // 0x00
+#define BLE_FAILURE                    SNP_FAILURE // 0x83
+#define BLE_INVALID_PARAMETERS         SNP_INVALID_PARAMS // 0x84
+#define BLE_ALREADY_ADVERTISING        SNP_ALREADY_ADVERTISING // 0x8A
+#define BLE_NOT_ADVERTISING            SNP_NOT_ADVERTISING // 0x8B
+#define BLE_NOT_CONNECTED              SNP_NOT_CONNECTED // 0x92
+#define BLE_INVALID_HANDLE             0x50
+#define BLE_UNDEFINED_VALUE            0x51
+#define BLE_NOT_IMPLEMENTED            0x52
+#define BLE_SERIAL_DISABLED            0x53
+#define BLE_TIMEOUT                    0x54
+#define BLE_CHECK_ERROR                0x55
 
 /* Type of connection between the user application's
    processor and the bluetooth chip */
-#define BLE_PORT_LOCAL          SAP_PORT_LOCAL // unsupported
-#define BLE_PORT_UART           SAP_PORT_REMOTE_UART
-#define BLE_PORT_SPI            SAP_PORT_REMOTE_SPI // unsupported
+#define BLE_PORT_LOCAL                 SAP_PORT_LOCAL // unsupported
+#define BLE_PORT_UART                  SAP_PORT_REMOTE_UART
+#define BLE_PORT_SPI                   SAP_PORT_REMOTE_SPI // unsupported
 
 /*
  * For setAdvertData.
@@ -33,11 +32,11 @@
  * The first two are passively seen by a client, while scanning is an
  * active action to request more data.
  */
-#define BLE_ADV_DATA_NOTCONN    SAP_ADV_DATA_NOTCONN
-#define BLE_ADV_DATA_CONN       SAP_ADV_DATA_CONN
-#define BLE_ADV_DATA_SCANRSP    SAP_ADV_DATA_SCANRSP
+#define BLE_ADV_DATA_NOTCONN           SAP_ADV_DATA_NOTCONN
+#define BLE_ADV_DATA_CONN              SAP_ADV_DATA_CONN
+#define BLE_ADV_DATA_SCANRSP           SAP_ADV_DATA_SCANRSP
 /* The number of kinds of advertising data */
-#define MAX_ADVERT_IDX          3
+#define MAX_ADVERT_IDX                 3
 
 /*
  * For startAdvert.
@@ -45,63 +44,63 @@
  * Scannable undirected advertisement.
  * Non-connectable undirected advertisement.
  */
-#define BLE_ADV_MODE_CONN               SNP_ADV_TYPE_CONN
-#define BLE_ADV_MODE_SCANABLE           SNP_ADV_TYPE_SCANABLE
-#define BLE_ADV_MODE_NONCONN            SNP_ADV_TYPE_NONCONN
+#define BLE_ADV_MODE_CONN              SNP_ADV_TYPE_CONN
+#define BLE_ADV_MODE_SCANABLE          SNP_ADV_TYPE_SCANABLE
+#define BLE_ADV_MODE_NONCONN           SNP_ADV_TYPE_NONCONN
 
 /* startAdvert connectedBehavior */
-#define BLE_ADV_STOP_ON_CONN            SNP_ADV_STOP_ON_CONN
-#define BLE_ADV_RESTART_ON_CONN_EST     SNP_ADV_RESTART_ON_CONN_EST
-#define BLE_ADV_RESTART_ON_CONN_TERM    SNP_ADV_RESTART_ON_CONN_TERM
+#define BLE_ADV_STOP_ON_CONN           SNP_ADV_STOP_ON_CONN
+#define BLE_ADV_RESTART_ON_CONN_EST    SNP_ADV_RESTART_ON_CONN_EST
+#define BLE_ADV_RESTART_ON_CONN_TERM   SNP_ADV_RESTART_ON_CONN_TERM
 
 /* Characteristic value properties */
-#define BLE_READABLE            SNP_GATT_PROP_READ
-#define BLE_WRITABLE_NORSP      SNP_GATT_PROP_WRITE_NORSP
-#define BLE_WRITABLE            SNP_GATT_PROP_WRITE
-#define BLE_NOTIFIABLE          SNP_GATT_PROP_NOTIFICATION
-#define BLE_INDICATABLE         SNP_GATT_PROP_INDICATION
+#define BLE_READABLE                   SNP_GATT_PROP_READ
+#define BLE_WRITABLE_NORSP             SNP_GATT_PROP_WRITE_NORSP
+#define BLE_WRITABLE                   SNP_GATT_PROP_WRITE
+#define BLE_NOTIFIABLE                 SNP_GATT_PROP_NOTIFICATION
+#define BLE_INDICATABLE                SNP_GATT_PROP_INDICATION
 
 /*
  * Security Parameters
  */
 /* Security Mode */
-#define BLE_SECURITY_NONE                     SAP_SECURITY_NONE
-#define BLE_SECURITY_WAIT_FOR_REQUEST         SAP_SECURITY_WAIT_FOR_REQUEST
-#define BLE_SECURITY_INITIATE_UPON_CONNECTION SAP_SECURITY_INITIATE_UPON_CONNECTION
+#define BLE_SECURITY_NONE                      SAP_SECURITY_NONE
+#define BLE_SECURITY_WAIT_FOR_REQUEST          SAP_SECURITY_WAIT_FOR_REQUEST
+#define BLE_SECURITY_INITIATE_UPON_CONNECTION  SAP_SECURITY_INITIATE_UPON_CONNECTION
 
 /* Security IO Capabilities */
-#define BLE_DISPLAY_ONLY                      SAP_DISPLAY_ONLY       // Display Only Device
-#define BLE_DISPLAY_YES_NO                    SAP_DISPLAY_YES_NO     // Display and Yes and No Capable
-#define BLE_KEYBOARD_ONLY                     SAP_KEYBOARD_ONLY      // Keyboard Only
-#define BLE_NO_INPUT_NO_OUTPUT                SAP_NO_INPUT_NO_OUTPUT // No Display or Input Device
-#define BLE_KEYBOARD_DISPLAY                  SAP_KEYBOARD_DISPLAY   // Both Keyboard and Display Capable
+#define BLE_DISPLAY_ONLY               SAP_DISPLAY_ONLY       // Display Only Device
+#define BLE_DISPLAY_YES_NO             SAP_DISPLAY_YES_NO     // Display and Yes and No Capable
+#define BLE_KEYBOARD_ONLY              SAP_KEYBOARD_ONLY      // Keyboard Only
+#define BLE_NO_INPUT_NO_OUTPUT         SAP_NO_INPUT_NO_OUTPUT // No Display or Input Device
+#define BLE_KEYBOARD_DISPLAY           SAP_KEYBOARD_DISPLAY   // Both Keyboard and Display Capable
 
 /* Security States */
-#define BLE_SECURITY_STATE_COMPLETE   SNP_GAPBOND_PAIRING_STATE_COMPLETE   // Pairing complete
-#define BLE_SECURITY_STATE_BONDED     SNP_GAPBOND_PAIRING_STATE_BONDED     // Devices bonded
-#define BLE_SECURITY_STATE_BOND_SAVED SNP_GAPBOND_PAIRING_STATE_BOND_SAVED // Bonding record saved in NV
+#define BLE_SECURITY_STATE_COMPLETE    SNP_GAPBOND_PAIRING_STATE_COMPLETE   // Pairing complete
+#define BLE_SECURITY_STATE_BONDED      SNP_GAPBOND_PAIRING_STATE_BONDED     // Devices bonded
+#define BLE_SECURITY_STATE_BOND_SAVED  SNP_GAPBOND_PAIRING_STATE_BOND_SAVED // Bonding record saved in NV
 
 /* Whitelist Parameters */
-#define BLE_WHITELIST_DISABLE SAP_WHITELIST_DISABLE // Enable White List usage
-#define BLE_WHITELIST_ENABLE  SAP_WHITELIST_ENABLE  // Disable White List usage
+#define BLE_WHITELIST_DISABLE          SAP_WHITELIST_DISABLE // Enable White List usage
+#define BLE_WHITELIST_ENABLE           SAP_WHITELIST_ENABLE  // Disable White List usage
 
 // Minimum connection interval (units of 1.25ms, 6=7.5ms) if automatic
 // parameter update request is enabled
-#define BLE_DEF_DESIRED_MIN_CONN_INT     6
+#define BLE_DEF_DESIRED_MIN_CONN_INT   6
 
 // Maximum connection interval (units of 1.25ms, 80=100ms) if automatic
 // parameter update request is enabled
-#define BLE_DEF_DESIRED_MAX_CONN_INT     80
+#define BLE_DEF_DESIRED_MAX_CONN_INT   80
 
  // Company Identifier: Texas Instruments Inc. (13)
-#define TI_COMPANY_ID                         0x000D
-#define TI_ST_DEVICE_ID                       0x03
-#define TI_ST_KEY_DATA_ID                     0x00
+#define TI_COMPANY_ID                  0x000D
+#define TI_ST_DEVICE_ID                0x03
+#define TI_ST_KEY_DATA_ID              0x00
 
 /*
  * Characteristic value formatting, Bluetooth spec Vol 3: Part G: 3.3.3.5.2
  */
-#define BLE_UINT                 0x06
+#define BLE_UINT                       0x06
 
 typedef struct
 {
