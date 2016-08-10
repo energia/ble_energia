3d5ef2b0b0fb31e27940f620b647b90876e2daa0
Andrew Blatner <a0226940@ti.com>
2016-08-10 14:04:24 -0700

    Reorganize BLELog.h

--- BLE/src/BLELog.h
+++ BLE/src/BLELog.h
@@ -8,20 +8,27 @@
 extern uint8_t logLevel;
 volatile extern bool apLogLock; // AP owns logging if true
 
+/* Set the task given ability to lock out the other. */
 void logSetAPTask(Task_Handle apTask);
 
-void logParam(const char name[], const uint8_t buf[], uint16_t len, bool isBigEnd=true);
-void logParam(const char name[], int value, int base=DEC);
-void logParam(const char name[], const char value[]);
-void logParam(const char value[]);
-void logUUID(const uint8_t UUID[], uint8_t len);
+/* Main logging functions that will call logAcquire(). */
 void logError(uint8_t status);
 void logError(const char msg[], uint8_t status);
 void logRPC(const char msg[]);
 void logAsync(const char name[], uint8_t cmd1);
 void logChar(const char action[]);
-void logReset(void);
+
+/* Secondary logging functions that only log if the last main function logs. */
+void logParam(const char name[], const uint8_t buf[], uint16_t len, bool isBigEnd=true);
+void logParam(const char name[], int value, int base=DEC);
+void logParam(const char name[], const char value[]);
+void logParam(const char value[]);
+void logUUID(const uint8_t UUID[], uint8_t len);
+
 void logAcquire(void);
 void logRelease(void);
+
+/* Used in ble.end(). */
+void logReset(void);
 
 #endif
